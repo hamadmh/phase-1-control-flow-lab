2c1cd0cea1b24531dcbc047cc6283c73208e7def
Mohammed Diary Mohammed <hamadiary1@gmail.com>
2022-11-13 16:49:53 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(hight){
+  if(hight<=400) return 'This one is on me!'
+  else if(hight>400 && hight<2000) return 'That will be twenty bucks.'
+  else if (hight>=2000 && hight <2500) return 'I will gladly take your thirty bucks.'
+  else if (hight>2500) return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === "NYC") return "Ok, sounds good."
+  else return "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return 'Thank you.'
+    default:
+     return "Bye."
+  }
 }
